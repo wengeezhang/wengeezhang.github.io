84d3f61a559c32c40282962acbc043967c56cc3d
wengeezhang <wengeezhang@gmail.com>
2015-03-25 17:28:21 +0800

    弹出消息

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
+self.addEventListener('push', function(event) {  
+  console.log('Received a push message', event);
 
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var icon = 'https://simple-push-demo.appspot.com/images/touch/icon-128x128.png';  
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      icon: icon,  
+      tag: tag  
+    })  
+  );  
+});
